4808f8b94c96fcd34d77b185b6f0b0e2ff3276ca
sam <sam@justcallmesam.com>
2024-12-28 17:26:59 -0500

    update social icon styles

--- src/app/components/SocialIcons.tsx
+++ src/app/components/SocialIcons.tsx
@@ -6,13 +6,13 @@ import { SocialIcon } from 'react-social-icons'
 const iconProps = {
     target: "_blank",
     rel: "noopener noreferrer",
-    bgColor: "transparent",
+    bgColor: "rgba(229,227,211,0.1)",
     fgColor: "rgba(229,227,211,0.8)",
 }
 
 const SocialIcons = () => {
     return (
-      <div className="absolute top-10 z-20 flex gap-3">
+      <div className="absolute top-10 z-20 flex gap-4">
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://open.spotify.com/artist/7xFkiQ9B6WA6932TniPRGr"
