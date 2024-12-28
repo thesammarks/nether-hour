bf0c2bf94b7d4ebc4ce4df84d9982f8471e8a09f
sam <sam@justcallmesam.com>
2024-12-28 16:04:23 -0500

    remove icon glitch and clean up links

--- src/app/components/SocialIcons.tsx
+++ src/app/components/SocialIcons.tsx
@@ -2,66 +2,46 @@
 
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
-import { useGlitch } from 'react-powerglitch'
 
 const iconProps = {
     target: "_blank",
     rel: "noopener noreferrer",
     bgColor: "transparent",
     fgColor: "rgba(229,227,211,0.8)",
-    // style: "width: 20px",
 }
 
 const SocialIcons = () => {
-    const glitch = useGlitch({
-        "playMode": "hover", 
-        "timing": {
-            "duration": 700
-        },
-        "shake": {
-            "velocity": 4,
-            "amplitudeX": 0.05,
-            "amplitudeY": 0.02
-        },
-    });
-    
     return (
       <div className="absolute top-10 z-20 flex gap-3">
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
-            ref={glitch.ref}
-            url="https://open.spotify.com/artist/7xFkiQ9B6WA6932TniPRGr/"
+            url="https://open.spotify.com/artist/7xFkiQ9B6WA6932TniPRGr"
             {...iconProps}
         />
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             network="itunes" 
-            ref={glitch.ref}
-            url="https://music.apple.com/us/artist/nether-hour/1544082556/"
+            url="https://music.apple.com/us/artist/nether-hour/1544082556"
             {...iconProps}
         />
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
-            ref={glitch.ref}
-            url="https://www.instagram.com/netherhour/"
+            url="https://www.instagram.com/netherhour"
             {...iconProps}
         />
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
-            ref={glitch.ref}
-            url="https://www.youtube.com/@netherhour/"
+            url="https://www.youtube.com/@netherhour"
             {...iconProps}
         />
         <SocialIcon
             className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
-            ref={glitch.ref}
-            url="https://x.com/NetherHour/"
+            url="https://x.com/NetherHour"
             {...iconProps}
         />
         <SocialIcon
             className="max-sm:hidden drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
-            ref={glitch.ref}
-            url="https://www.facebook.com/thenetherhour/"
+            url="https://www.facebook.com/thenetherhour"
             {...iconProps}
         />
       </div>
