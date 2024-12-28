07f7bc83ad1deca1e0c1531b68ccce3b8a7d7bf8
sam <sam@justcallmesam.com>
2024-12-28 17:37:19 -0500

    consolidate tw styles

--- src/app/components/SocialIcons.tsx
+++ src/app/components/SocialIcons.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 
 const iconProps = {
+    className: "drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]",
     target: "_blank",
     rel: "noopener noreferrer",
-    bgColor: "rgba(229,227,211,0.1)",
+    bgColor: "rgba(0,0,0,0.1)",
     fgColor: "rgba(229,227,211,0.8)",
 }
 
@@ -14,35 +15,30 @@ const SocialIcons = () => {
     return (
       <div className="absolute top-10 z-20 flex gap-4">
         <SocialIcon
-            className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://open.spotify.com/artist/7xFkiQ9B6WA6932TniPRGr"
             {...iconProps}
         />
         <SocialIcon
-            className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             network="itunes" 
             url="https://music.apple.com/us/artist/nether-hour/1544082556"
             {...iconProps}
         />
         <SocialIcon
-            className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://www.instagram.com/netherhour"
             {...iconProps}
         />
         <SocialIcon
-            className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://www.youtube.com/@netherhour"
             {...iconProps}
         />
         <SocialIcon
-            className="drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://x.com/NetherHour"
             {...iconProps}
         />
         <SocialIcon
-            className="max-sm:hidden drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
             url="https://www.facebook.com/thenetherhour"
             {...iconProps}
+            className="max-sm:hidden drop-shadow-[0px_0px_4px_rgba(255,255,255,0.5)]"
         />
       </div>
     );
