ab03c7e07445c3709b0ac27af068350cf9f4a26a
sam <sam@justcallmesam.com>
2024-12-29 12:43:18 -0500

    add static scrollbar for scrollable content sections

--- src/app/components/ListenNow/SpotifyLinkCards.tsx
+++ src/app/components/ListenNow/SpotifyLinkCards.tsx
@@ -1,48 +1,50 @@
+import { Progress } from "@nextui-org/react";
 import SpotifyLinkCard from "./SpotifyLinkCard";
 
 const SpotifyLinkCards = () => {
     return (
         <div className="
-            flex flex-nowrap gap-5 max-w-[75%]
-            rounded-xl overflow-x-auto px-4 pt-4 pb-6
+            relative flex max-w-[75%] rounded-xl px-4 pt-4 pb-2
             bg-gradient-to-b from-neutral-700 to-neutral-600
         ">
-            <SpotifyLinkCard
-                title="Look My Way"
-                subtitle="2024 • Single"
-                url="https://open.spotify.com/album/2ITCfEO8ZYCIbOe8QVLiB0"
-                img="/images/look_my_way.jpg"
-            />
-            <SpotifyLinkCard
-                title="Long As I Ain't Alone"
-                subtitle="2024 • Single"
-                url="https://open.spotify.com/album/1T5NTy3lHhEz2O9Aq1Q6s4"
-                img="/images/long_as_i_aint_alone.jpg"
-            />
-            <SpotifyLinkCard
-                title="This is Nether Hour"
-                subtitle="Playlist"
-                url="https://open.spotify.com/playlist/37i9dQZF1DZ06evO4sV7mt"
-                img="/images/this_is_nether_hour.jpg"
-            />
-            <SpotifyLinkCard
-                title="SUNSHINE"
-                subtitle="2024 • Album"
-                url="https://open.spotify.com/album/0CJY4hih3MeO8rmCpbxxHt"
-                img="/images/sunshine.jpg"
-            />
-            <SpotifyLinkCard
-                title="Rattlesnake Gumbo"
-                subtitle="2023 • Album"
-                url="https://open.spotify.com/album/4FbVIUqviL1clsbL1Ql3wS"
-                img="/images/rattlesnake_gumbo.jpg"
-            />
-            <SpotifyLinkCard
-                title="The Hit"
-                subtitle="2021 • Single"
-                url="https://open.spotify.com/album/4FbVIUq3VsBuJJqFL1s20QB87J8AAviL1clsbL1Ql3wS"
-                img="/images/the_hit.jpg"
-            />
+            <div className="flex flex-nowrap gap-5 pb-4 overflow-x-scroll my-scrollbar">
+                <SpotifyLinkCard
+                    title="Look My Way"
+                    subtitle="2024 • Single"
+                    url="https://open.spotify.com/album/2ITCfEO8ZYCIbOe8QVLiB0"
+                    img="/images/look_my_way.jpg"
+                />
+                <SpotifyLinkCard
+                    title="Long As I Ain't Alone"
+                    subtitle="2024 • Single"
+                    url="https://open.spotify.com/album/1T5NTy3lHhEz2O9Aq1Q6s4"
+                    img="/images/long_as_i_aint_alone.jpg"
+                />
+                <SpotifyLinkCard
+                    title="This is Nether Hour"
+                    subtitle="Playlist"
+                    url="https://open.spotify.com/playlist/37i9dQZF1DZ06evO4sV7mt"
+                    img="/images/this_is_nether_hour.jpg"
+                />
+                <SpotifyLinkCard
+                    title="SUNSHINE"
+                    subtitle="2024 • Album"
+                    url="https://open.spotify.com/album/0CJY4hih3MeO8rmCpbxxHt"
+                    img="/images/sunshine.jpg"
+                />
+                <SpotifyLinkCard
+                    title="Rattlesnake Gumbo"
+                    subtitle="2023 • Album"
+                    url="https://open.spotify.com/album/4FbVIUqviL1clsbL1Ql3wS"
+                    img="/images/rattlesnake_gumbo.jpg"
+                />
+                <SpotifyLinkCard
+                    title="The Hit"
+                    subtitle="2021 • Single"
+                    url="https://open.spotify.com/album/4FbVIUq3VsBuJJqFL1s20QB87J8AAviL1clsbL1Ql3wS"
+                    img="/images/the_hit.jpg"
+                />
+            </div>
         </div>
     );
 };
--- src/app/components/Upcoming/InfoCards.tsx
+++ src/app/components/Upcoming/InfoCards.tsx
@@ -6,37 +6,38 @@ import InfoCard from "./InfoCard";
 const InfoCards = () => {
     return (
         <div className="
-            flex flex-nowrap gap-5 max-w-[75%] 
-            rounded-xl overflow-x-auto px-4 py-4 max-xl:pb-6
+            relative flex max-w-[75%] rounded-xl px-4 pt-4 pb-2
             bg-gradient-to-b from-neutral-700 to-neutral-600
         ">
-            <InfoCard
-                header="Get Tickets"
-                title="Nether Hour"
-                icon={ExternalLink}
-                venue="Fox Theatre - Boulder, CO"
-                dates="Friday, Jan 31, 2025 - 8 P.M."
-                url="https://www.eventticketscenter.com/nether-hour-boulder-01-31-2025/6784804/t"
-                img="/images/fox_theatre_flyer.jpg"
-            />
-            <InfoCard
-                header="Get Tickets"
-                title="Nether Hour & Horse Bitch"
-                icon={ExternalLink}
-                venue="Aggie Theatre - Fort Collins, CO"
-                dates="Saturday, Feb 1, 2025 - 8 P.M."
-                url="https://www.eventticketscenter.com/nether-hour-horse-bitch-fort-collins-02-01-2025/6783064/t"
-                img="/images/aggie_theatre_flyer.jpg"
-            />
-            <InfoCard
-                header="Latest Updates"
-                title="Follow @netherhour"
-                icon={Instagram} 
-                venue="on Instagram for the latest"
-                dates="announcements and information."
-                url="https://instagram.com/netherhour"
-                img="/images/nh_profile.jpg"
-            />
+            <div className="flex flex-nowrap gap-5 max-xl:pb-4 overflow-x-scroll my-scrollbar">
+                <InfoCard
+                    header="Get Tickets"
+                    title="Nether Hour"
+                    icon={ExternalLink}
+                    venue="Fox Theatre - Boulder, CO"
+                    dates="Friday, Jan 31, 2025 - 8 P.M."
+                    url="https://www.eventticketscenter.com/nether-hour-boulder-01-31-2025/6784804/t"
+                    img="/images/fox_theatre_flyer.jpg"
+                />
+                <InfoCard
+                    header="Get Tickets"
+                    title="Nether Hour & Horse Bitch"
+                    icon={ExternalLink}
+                    venue="Aggie Theatre - Fort Collins, CO"
+                    dates="Saturday, Feb 1, 2025 - 8 P.M."
+                    url="https://www.eventticketscenter.com/nether-hour-horse-bitch-fort-collins-02-01-2025/6783064/t"
+                    img="/images/aggie_theatre_flyer.jpg"
+                />
+                <InfoCard
+                    header="Latest Updates"
+                    title="Follow @netherhour"
+                    icon={Instagram} 
+                    venue="on Instagram for the latest"
+                    dates="announcements and information."
+                    url="https://instagram.com/netherhour"
+                    img="/images/nh_profile.jpg"
+                />
+            </div>
         </div>
     );
 };
