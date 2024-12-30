20568092b3c5135013dc2a40a1f67f2bbd2bb67c
Sam <sam@justcallmesam.com>
2024-12-30 03:16:56 -0500

    broken link

--- src/app/components/Upcoming/Upcoming.tsx
+++ src/app/components/Upcoming/Upcoming.tsx
@@ -4,12 +4,11 @@ const Upcoming = () => {
     return (
         <div className="flex flex-col w-full items-center gap-5">
             <div className="text-2xl font-bold">Upcoming</div>
-            <div className="text-center max-w-[300px]">
+            <div className="text-center max-w-[325px]">
                 Follow{' '}
-                <a className="font-bold text-[#f40e2d]" href="https://instgram.com/netherhour">@netherhour</a>{' '}
-                for latest announcements and show information and email{' '}
-                <a className="font-bold text-[#f40e2d]" href="mailto:netherhour@gmail.com">netherhour@gmail.com</a>{' '}
-                for booking and business inquires.
+                <a target="_blank" rel="noopener noreferrer" className="font-bold text-[#f40e2d]" href="https://instagram.com/netherhour">@netherhour</a>{' '}
+                on Instagram for the latest announcements and show information. For booking and business inquiries, email{' '}
+                <a target="_blank" rel="noopener noreferrer" className="font-bold text-[#f40e2d]" href="mailto:netherhour@gmail.com">netherhour@gmail.com</a>.
             </div>
             <InfoCards />
         </div>
