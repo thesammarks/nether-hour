637c8140c58707369012d93050ed814fb0e44b82
Sam <sam@justcallmesam.com>
2025-01-01 19:18:16 -0600

    add vercel speed insights dependency and add to `providers.tsx`

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,13 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       {children}
       <Analytics />
+      <SpeedInsights />
     </NextUIProvider>
   )
 }
