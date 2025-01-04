425f4f57d7ab81216adace8f1356fa22919b79a8
Sam <sam@justcallmesam.com>
2025-01-04 00:24:57 -0600

    update site metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
-import '@mantine/core/styles.layer.css';
 import '@mantine/core/styles.layer.css';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,12 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta property="og:title" content="Nether Hour"/>
-        <meta property="og:description" content="A band based in Austin, TX. Follow @netherhour on Instagram."/>
+        <meta property="og:description" content="Follow @netherhour on Instagram for the latest announcements and show information. For booking and business inquiries, email netherhour@gmail.com."/>
         <meta property="og:image" content="images/nh_profile.jpg" />
         <meta property="og:type" content="article"/>
 
         <title>Nether Hour</title>
-        <meta name="description" content="A band based in Austin, TX. Follow @netherhour on Instagram."/>
+        <meta name="description" content="A band based in Austin, TX. Follow @netherhour on Instagram for the latest announcements and show information. For booking and business inquiries, email netherhour@gmail.com."/>
         <link rel="icon" href="/favicon.ico"/>
       </head>
       <body className={inter.className}>
