ccd76ecb79c9bb95e1d18e391a154686a1ef8444
Sam <sam@justcallmesam.com>
2025-01-03 18:47:44 -0600

    conform tab spacing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,10 @@ import { NextUIProvider } from '@nextui-org/react'
 import { Analytics } from "@vercel/analytics/react"
 
 export function Providers({children}: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      {children}
-      <Analytics />
-    </NextUIProvider>
-  )
+    return (
+        <NextUIProvider>
+            {children}
+        <Analytics />
+        </NextUIProvider>
+    )
 }
