99f42c472bd26f6fbb14a5950c7e3c371e692b6e
Sam <sam@justcallmesam.com>
2025-01-03 18:49:22 -0600

    conform tab spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,22 @@ import React from "react";
 import Upcoming from "./components/Upcoming/Upcoming";
 
 export default function Home() {  
-  return (
-    <main className="min-h-screen bg-black text-neutral-200 flex justify-center items-center">
-      <VideoBkgd />
-      <SocialIcons />
-      <NhLogo />
-      <ScrollIcon />
+    return (
+        <main className="min-h-screen bg-black text-neutral-200 flex justify-center items-center">
+            <VideoBkgd />
+            <SocialIcons />
+            <NhLogo />
+            <ScrollIcon />
 
-      <div className="relative bg-black w-full top-[110vh] flex flex-col items-center gap-10 z-20 pb-16">
-        <Upcoming />
-        <TheBand />
-        <ListenNow />
+            <div className="relative bg-black w-full top-[110vh] flex flex-col items-center gap-10 z-20 pb-16">
+                <Upcoming />
+                <TheBand />
+                <ListenNow />
 
-        <footer className="flex text-sm">
-          © 2024 Nether Family Records | website by&nbsp;<b><a className="text-[#f40e2d]" href="https://justcallmesam.com" target="_blank" rel="noopener noreferrer">sam</a></b>
-        </footer>
-      </div>
-    </main>
-  );
+                <footer className="flex text-sm">
+                    © 2024 Nether Family Records | website by&nbsp;<b><a className="text-[#f40e2d]" href="https://justcallmesam.com" target="_blank" rel="noopener noreferrer">sam</a></b>
+                </footer>
+            </div>
+        </main>
+    );
 }
