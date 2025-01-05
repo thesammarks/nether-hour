28c8b42ab3db29897cbb72e21cde096f6400a6ef
Sam <sam@justcallmesam.com>
2025-01-04 21:21:53 -0600

    add vercel speed insights provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,14 @@
 import {Analytics} from "@vercel/analytics/react"
 import {MantineProvider} from "@mantine/core";
 import {theme} from '@/mantine-theme';
+import {SpeedInsights} from "@vercel/speed-insights/next"
 
 export function Providers({children}: { children: React.ReactNode }) {
     return (
         <MantineProvider defaultColorScheme="auto" theme={theme}>
             {children}
             <Analytics />
+            <SpeedInsights />
         </MantineProvider>
     )
 }
