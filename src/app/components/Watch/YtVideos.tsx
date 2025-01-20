a67cc21ee40ea5ca6411c63d04943e00f0eb66a3
Sam <sam@justcallmesam.com>
2025-01-19 21:24:07 -0600

    yt container spacing + bg color

--- src/app/components/Watch/YtVideos.tsx
+++ src/app/components/Watch/YtVideos.tsx
@@ -4,9 +4,9 @@ export default function YtVideos() {
     return (
         <div className="
             flex flex-col w-full
-            gap-4 justify-items-center
-            px-4 py-4 rounded-xl
-            bg-gradient-to-b from-neutral-700 to-neutral-600
+            gap-6 justify-items-center
+            px-6 py-6 rounded-xl
+            bg-neutral-900
         ">
             <YtVideo ytId="RP2C4aKWtXc" />
             <YtVideo ytId="f4IX070ZQf8" />
