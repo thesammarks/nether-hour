961840c469733888cd9827ddac57535d2c83078a
Sam <sam@justcallmesam.com>
2025-01-19 23:50:51 -0600

    optimize imports

--- src/app/components/ScrollIcon.tsx
+++ src/app/components/ScrollIcon.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react'
 
 const ScrollIcon = () => {
     const [isFading, setIsFading] = useState(false);
