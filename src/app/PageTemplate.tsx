dea752243bec60411b58a87ad56d466396408480
Sam <sam@justcallmesam.com>
2025-01-22 16:49:54 -0600

    add page template and apply to ABOUT and PRESS pages

---
+++ src/app/PageTemplate.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NhPageLink from "@/app/components/NhPageLink";
+import Footer from "@/app/components/Footer";
+import Nav from "@/app/components/Nav/Nav";
+import styles from "./PageTemplate.module.scss";
+
+interface PageTemplateProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function PageTemplate({ children, className }: PageTemplateProps) {
+    return (
+        <main className={`${styles.page} ${className}`}>
+            <Nav />
+            <NhPageLink />
+            {children}
+            <Footer />
+        </main>
+    );
+}
--- src/app/press/page.tsx
+++ src/app/press/page.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import NhPageLink from "@/app/components/NhPageLink";
-import Footer from "@/app/components/Footer";
 import Press from "@/app/press/Press";
-import Nav from "@/app/components/Nav/Nav";
+import PageTemplate from "@/app/PageTemplate";
 
-export default function Home() {
+export default function Page() {
     return (
-        <main className="
-            min-h-screen pb-10 w-full
-            text-neutral-200 bg-neutral-900
-            flex flex-col justify-center items-center
-            pt-[125px] min-[880px]:pt-[75px]
-        ">
-            <Nav />
-            <NhPageLink />
+        <PageTemplate>
             <Press />
-            <Footer />
-        </main>
+        </PageTemplate>
     );
 }
