0258f1605a820491ca2428a8d2aff0ecb62b1165
Sam <sam@justcallmesam.com>
2025-01-31 13:52:19 -0600

    `Footer` copyright year

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "./Footer.module.scss";
 export default function Footer() {
     return (
         <footer className={styles.container}>
-            <span>© 2024 Nether Family Records</span>
+            <span>© 2025 Nether Family Records</span>
             <span>
                 Website by&nbsp;
                 <b>
