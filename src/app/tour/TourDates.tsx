61092eddb7d31b019c435f020b81498c6074da55
Sam <sam@justcallmesam.com>
2025-03-08 15:04:19 -0600

    remove past tour dates + cycle assets

--- src/app/tour/TourDates.tsx
+++ src/app/tour/TourDates.tsx
@@ -6,38 +6,6 @@ export default function TourDates() {
     return (
         <div className="w-full flex flex-col gap-5 justify-center items-center">
             <TourDate
-                date="Feb 20, 2025"
-                city="Nashville, TN"
-                title="Nether Hour"
-                venue="Dee's Country Cocktail Lounge"
-                url="https://tunehatch.com/shows/139176608"
-                img="/images/tour/feb_20_dees_country_cocktail_lounge.jpg"
-            />
-            <TourDate
-                date="Feb 27, 2025"
-                city="Austin, TX"
-                title="Nether Hour w/ Underground Springhouse"
-                venue="Sagebrush"
-                url="https://sagebrushtexas.com"
-                img="/images/tour/feb_27_mohawk.jpg"
-            />
-            <TourDate
-                date="Feb 28, 2025"
-                city="Fort Worth, TX"
-                title="Nether Hour w/ Underground Springhouse"
-                venue="The Cicada"
-                url="https://thecicadafw.com"
-                img="/images/tour/moonshine_tour.jpg"
-            />
-            <TourDate
-                date="March 1, 2025"
-                city="Dallas, TX"
-                title="Nether Hour w/ Underground Springhouse"
-                venue="The Double Wide"
-                url="https://www.prekindle.com/event/78768-underground-springhouse-nether-hour-dallas"
-                img="/images/tour/feb_28_mar_1_cicada_double_wide.jpg"
-            />
-            <TourDate
                 date="April 4, 2025"
                 city="New Orleans, LA"
                 title="Nether Hour"
